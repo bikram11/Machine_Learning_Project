2560a54f2351cbd49836738fa23b87b035a01d19
bikram11 <bekrumed1@gmail.com>
2023-04-12 16:10:37 -0400

    first commit

---
+++ convert_to_image.py
@@ -0,0 +1,58 @@
+import os
+import cv2
+
+basepath = './BDDA/test'
+entries = os.listdir(basepath)
+entries.sort()
+
+camera_image_path = 'test/camera_images'
+gazemap_path = 'test/gazemap_images'
+
+if not os.path.exists(gazemap_path):
+    os.makedirs(gazemap_path)
+
+
+if not os.path.exists(camera_image_path):
+    os.makedirs(camera_image_path)
+
+for each_folder in entries:
+    if "gazemap" in each_folder:
+        inner_entries = os.listdir(basepath+"/"+each_folder)
+        inner_entries.sort()
+        for inner_files in inner_entries:
+            videocap = cv2.VideoCapture(basepath+"/"+each_folder+"/"+inner_files)
+            
+            count = 0
+            while(True):
+                ret, frame = videocap.read()
+                if ret:
+                    cv2.imwrite(gazemap_path+'/%d_pure_hm_%d.jpg'%(int(inner_files[:-4]),count),frame)
+                    count +=1
+                else:
+                    break
+            
+            videocap.release()
+
+    if "camera" in each_folder:
+        inner_entries = os.listdir(basepath+"/"+each_folder)
+        inner_entries.sort()
+        for inner_files in inner_entries:
+            videocap = cv2.VideoCapture(basepath+"/"+each_folder+"/"+inner_files)
+            
+            count = 0
+            while(True):
+                ret, frame = videocap.read()
+                if ret:
+                    cv2.imwrite(camera_image_path+'/%d_%d.jpg'%(int(inner_files[:-4]),count),frame)
+                    count +=1
+                else:
+                    break
+            
+            videocap.release()
+     
+            
+
+
+
+
+
---
+++ unzip_dataset.py
@@ -0,0 +1,5 @@
+# https://gmuedu-my.sharepoint.com/:u:/g/personal/badhika5_gmu_edu/EUlCIlqulltJvh-ZmIjfiRQBDPZ8CiS9SHRN844ovQoWXw?e=gK4WMn to download the BDD dataset
+
+import zipfile
+with zipfile.ZipFile("/home/bikram/Downloads/BDDA.zip","r") as zip_ref:
+    zip_ref.extractall("")

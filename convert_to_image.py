a35acf3f70b25cb7b8983a81ce6889bce2e86eff
bikram11 <bekrumed1@gmail.com>
2023-04-13 16:03:13 -0400

    generic convert to image and create gridmap script

--- convert_to_image.py
+++ convert_to_image.py
@@ -1,12 +1,12 @@
 import os
 import cv2
 
-basepath = './BDDA/test'
+basepath = 'BDDA/test'
 entries = os.listdir(basepath)
 entries.sort()
 
-camera_image_path = 'test/camera_images'
-gazemap_path = 'test/gazemap_images'
+camera_image_path = 'raw_data/test/camera_images'
+gazemap_path = 'raw_data/test/gazemap_images'
 
 if not os.path.exists(gazemap_path):
     os.makedirs(gazemap_path)
@@ -16,7 +16,8 @@ if not os.path.exists(camera_image_path):
     os.makedirs(camera_image_path)
 
 for each_folder in entries:
-    if "gazemap" in each_folder:
+    if "videos" in each_folder:
+        source_prefix = each_folder.split('_')
         inner_entries = os.listdir(basepath+"/"+each_folder)
         inner_entries.sort()
         for inner_files in inner_entries:
@@ -26,29 +27,16 @@ for each_folder in entries:
             while(True and count<10):
                 ret, frame = videocap.read()
                 if ret:
-                    cv2.imwrite(gazemap_path+'/%d_pure_hm_%d.jpg'%(int(inner_files[:-12]),count),frame)
+                    if(source_prefix[0]=="gazemap"):
+                        cv2.imwrite(gazemap_path+'/%d_%d.jpg'%(int(inner_files[:-12]),count),frame)
+                    else:
+                        cv2.imwrite(camera_image_path+'/%d_%d.jpg'%(int(inner_files[:-4]),count),frame)
                     count +=1
                 else:
                     break
             
             videocap.release()
 
-    if "camera" in each_folder:
-        inner_entries = os.listdir(basepath+"/"+each_folder)
-        inner_entries.sort()
-        for inner_files in inner_entries:
-            videocap = cv2.VideoCapture(basepath+"/"+each_folder+"/"+inner_files)
-            
-            count = 0
-            while(True and count<10):
-                ret, frame = videocap.read()
-                if ret:
-                    cv2.imwrite(camera_image_path+'/%d_%d.jpg'%(int(inner_files[:-4]),count),frame)
-                    count +=1
-                else:
-                    break
-            
-            videocap.release()
      
             
 
